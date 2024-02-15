0d3f4327da3397d6c58829e4c3a00b93d4efaa63
Andrii Zakutko <andrii.zakutko@nixs.com>
2024-02-15 10:12:42 +0100

    API, Application and Infrastructure projects were added in solution

---
+++ Infrastructure/Extensions/ActionResultExtensions.cs
@@ -0,0 +1,42 @@
+﻿using System.Net;
+using Domain.Results;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Infrastructure.Extensions;
+
+public static class ActionResultExtensions
+{
+    public static ActionResult HandleResult<T>(this ActionResult action, Result<T> result)
+    {
+        return result.State switch
+        {
+            ResultState.Ok => new OkObjectResult(result.Value),
+            ResultState.NotFound => CreateResponse(HttpStatusCode.NotFound, result.Error),
+            ResultState.BadRequest => new BadRequestObjectResult(result.Error),
+            ResultState.Unauthorized => new UnauthorizedObjectResult(result.Error),
+            _ => CreateResponse(HttpStatusCode.InternalServerError, result.Error)
+        };
+    }
+    
+    public static ActionResult HandleResult(this ActionResult action, Result result)
+    {
+        return result.State switch
+        {
+            ResultState.Ok => CreateResponse(HttpStatusCode.OK),
+            ResultState.NotFound => CreateResponse(HttpStatusCode.NotFound, result.Error),
+            ResultState.BadRequest => new BadRequestObjectResult(result.Error),
+            ResultState.Unauthorized => new UnauthorizedObjectResult(result.Error),
+            _ => CreateResponse(HttpStatusCode.InternalServerError, result.Error)
+        };
+    }
+    
+    private static ActionResult CreateResponse(HttpStatusCode statusCode, Error error = null)
+    {
+        var response = new ObjectResult(error)
+        {
+            StatusCode = (int)statusCode
+        };
+
+        return response;
+    }
+}
