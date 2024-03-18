d6587f01962b9ffe4a754c6ced99c3f08accaa08
Andrii Zakutko <andrii.zakutko@nixs.com>
2024-03-18 17:41:49 +0100

    delete unused comments

--- Domain/Transport/TransportAdvertisement.cs
+++ Domain/Transport/TransportAdvertisement.cs
@@ -4,18 +4,18 @@ namespace Domain.Transport;
 
 public class TransportAdvertisement : Advertisement
 {
-    public virtual TransportType Type { get; set; } // Тип транспорта
-    public virtual TransportMake Make { get; set; } // Марка авто
-    public virtual TransportModel Model { get; set; } // Модель
-    public double EngineCapacity { get; set; } // Объем двигателя
-    public string SerialNumber { get; set; } // Серийный номер
-    public double FuelConsumption { get; set; } // Расход двигателя
-    public string Country { get; set; } // Страна продажи
-    public string City { get; set; } // Город продажи
-    public string Address { get; set; } // Адрес
-    public int Mileage { get; set; } // Пробег
-    public string ManufactureCountry { get; set; } // Страна проихводитель
-    public DateTime ManufactureDate { get; set; } // Дата выпуска авто
+    public virtual TransportType Type { get; set; }
+    public virtual TransportMake Make { get; set; }
+    public virtual TransportModel Model { get; set; }
+    public double EngineCapacity { get; set; }
+    public string SerialNumber { get; set; }
+    public double FuelConsumption { get; set; }
+    public string Country { get; set; }
+    public string City { get; set; }
+    public string Address { get; set; }
+    public int Mileage { get; set; }
+    public string ManufactureCountry { get; set; }
+    public DateTime ManufactureDate { get; set; }
     public bool IsElectric { get; set; }
     public bool IsNew { get; set; }
     public bool IsUsed { get; set; }
