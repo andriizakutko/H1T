4b82a98819721201061e4c1ff2584f59ad0d246b
Andrii Zakutko <andrii.zakutko@nixs.com>
2024-03-20 12:06:34 +0100

    rename TransportController to ResourceController

--- API/Controllers/ResourceController.cs
+++ API/Controllers/ResourceController.cs
@@ -4,31 +4,31 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers;
 
-[Route("api/transport")]
-public class TransportController(ITransportService service) : BaseApiController
+[Route("api/recource")]
+public class ResourceController(ITransportService service) : BaseApiController
 {
-    [HttpGet("get-types")]
+    [HttpGet("get-transport-types")]
     [HasPermission(Permissions.User)]
     public async Task<IActionResult> GetTransportTypes()
     {
         return HandleResult(await service.GetTransportTypes());
     }
 
-    [HttpGet("get-makes/{transportTypeId}")]
+    [HttpGet("get-transport-makes/{transportTypeId}")]
     [HasPermission(Permissions.User)]
     public async Task<IActionResult> GetTransportMakesByTransportTypeId(Guid transportTypeId)
     {
         return HandleResult(await service.GetTransportMakesByTransportTypeId(transportTypeId));
     }
     
-    [HttpGet("get-body-types/{transportTypeId}")]
+    [HttpGet("get-transport-body-types/{transportTypeId}")]
     [HasPermission(Permissions.User)]
     public async Task<IActionResult> GetTransportBodyTypesByTransportTypeId(Guid transportTypeId)
     {
         return HandleResult(await service.GetTransportBodyTypesByTransportTypeId(transportTypeId));
     }
     
-    [HttpGet("get-models/{transportMakeId}")]
+    [HttpGet("get-transport-models/{transportMakeId}")]
     [HasPermission(Permissions.User)]
     public async Task<IActionResult> GetTransportModelsByTransportMakeId(Guid transportMakeId)
     {
